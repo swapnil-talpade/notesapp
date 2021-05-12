8cea1b636eeb8c09857445903368806e6e2813a1
Swapnil Talpade <swapniltalpade41@gmail.com>
2021-05-12 22:05:52 +0530

    Done with Addding and Deleting functionalities of notes along with other validation changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+import NotesList from './components/NotesList'
 
-function App() {
+
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+
+  const addNote = (text) => {
+    console.log(text)
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes)
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
